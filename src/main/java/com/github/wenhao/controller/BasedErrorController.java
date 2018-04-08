1291be68182e524c3c34463f751f92a0b7df8550
Hao Wen <wenhao@126.com>
2018-04-08 19:05:37 +0800

    [Wen Hao] add error handling.

--- src/main/java/com/github/wenhao/controller/BasedErrorController.java
+++ src/main/java/com/github/wenhao/controller/BasedErrorController.java
@@ -1,14 +1,12 @@
 package com.github.wenhao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -19,28 +17,24 @@ import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("${server.error.path:${error.path:/error}}")
-public class BasedErrorController implements ErrorController {
-
-    private ErrorAttributes errorAttributes;
+public class BasedErrorController extends AbstractErrorController {
 
     @Autowired
     public BasedErrorController(ErrorAttributes errorAttributes) {
-        this.errorAttributes = errorAttributes;
+        super(errorAttributes);
     }
 
     @RequestMapping
     public ResponseEntity error(HttpServletRequest request) {
-        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-        Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(requestAttributes, false);
+        Map<String, Object> errorAttributes = getErrorAttributes(request, false);
         List<FieldError> errors = (List) errorAttributes.get("errors");
         List<String> errorMessages = errors.stream()
                 .map(error -> error.getDefaultMessage())
                 .collect(toList());
-        int status = (int) errorAttributes.get("status");
         Map<String, List<String>> result = new HashMap<String, List<String>>() {{
             put("errors", errorMessages);
         }};
-        return ResponseEntity.status(status).body(result);
+        return ResponseEntity.status(getStatus(request)).body(result);
     }
 
     @Override
